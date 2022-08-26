3c9088deb394e45b6c648f82eb49993233e3682f
unknown <igormelo9001@gmail.com>
2022-08-26 19:21:43 -0300

    aplicacao completa

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.helloworld
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.media.MediaPlayer
+import android.net.Uri
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var play=findViewById<Button>(R.id.play)
+        play.setText("Play")
+        var stop=findViewById<Button>(R.id.stop)
+        stop.setText("Stop")
+        var mp=MediaPlayer()
+        play.setOnClickListener {
+            mp.setDataSource(this, Uri.parse("android.resource://"+this.packageName+"/"+R.raw.bladerunner))
+            mp.prepare()
+            mp.start()
+        }
+
+        stop.setOnClickListener {
+            mp.stop()
+            mp.release()
+            mp=MediaPlayer()
+        }
+
+        val button: Button = findViewById(R.id.mongol)
+        button.setOnClickListener {
+            val intent = Intent(this, mongolPeach::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+}
